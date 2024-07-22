cfeea40dcd758528eb5fe24f1cd32ba8f0872b28
Hrant <hrant.simonyan.2018@gmail.com>
2024-07-22 09:36:13 +0400

    create login component

---
+++ frontend/src/components/Login/index.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+import { FormValues } from './types';
+import Modal from '../Modal';
+import Input from '../Input';
+
+import styles from './index.module.css';
+
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export default function Login() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const methods = useForm<FormValues>({ mode: 'onChange' });
+  const { handleSubmit, reset } = methods;
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    console.log(data);
+  };
+  return (
+    <>
+      <button className={styles.button} onClick={openModal}>
+        Sign In
+      </button>
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        <h1 className={styles.title}>Login Form</h1>
+        <FormProvider {...methods}>
+          <form onSubmit={handleSubmit(onSubmit)} noValidate>
+            <Input
+              name="email"
+              type="email"
+              rules={{
+                required: 'Email is required',
+                pattern: {
+                  value: emailPattern,
+                  message: 'Invalid email address format. Please enter a valid email address.',
+                },
+              }}
+              placeholder="Email"
+            />
+            <Input
+              name="password"
+              type="password"
+              rules={{
+                required: 'Password is required',
+              }}
+              placeholder="Password"
+            />
+
+            <div className={styles.form_button_block}>
+              <input className={styles.form_button} type="submit" />
+              <input className={styles.form_button} type="button" onClick={() => reset()} value="reset" />
+            </div>
+          </form>
+        </FormProvider>
+      </Modal>
+    </>
+  );
+}
