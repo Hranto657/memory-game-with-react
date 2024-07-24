2b0650fe1cd496c3f7dc587508bbb145c1d33083
Hrant <hrant.simonyan.2018@gmail.com>
2024-07-24 10:23:36 +0400

    modify design using figma

--- frontend/src/components/Board/Card/index.tsx
+++ frontend/src/components/Board/Card/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ICard } from './types';
+import icon from '@/assets/card-back-icon.png';
 
 import styles from './index.module.css';
 
@@ -16,7 +17,9 @@ const Card = ({ id, image, alt, isFlipped, isMatched, onClick }: ICard) => {
         <div className={styles.card_front}>
           <img className={styles.card_front_img} src={image} alt={alt} />
         </div>
-        <div className={styles.card_back} />
+        <div className={styles.card_back}>
+          <img className={styles.card_back_img} src={icon} alt="Card Back Icon" />
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/Board/GameInfo/types.ts
+++
@@ -1,6 +0,0 @@
-import { CardType } from "@/types/commonTypes";
-
-export interface IGameInfo {
-  matchedCards: CardType[];
-  onReset: () => void;
-}
