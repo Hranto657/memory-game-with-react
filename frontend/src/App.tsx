b51d7b45755583db05ed0707651a8304148453b5
Hrant <hrant.simonyan.2018@gmail.com>
2024-08-09 17:43:48 +0400

    add react router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Container from './components/Container';
 import Board from './components/Board';
 
 function App() {
   return (
-    <Container>
-      <Board />
-    </Container>
+    <Router>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Navigate to="/level/1" />} />
+          <Route path="/level/:level" element={<Board />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
