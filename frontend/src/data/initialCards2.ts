71eb03214c6c06071995e1c75167e17fc7fb6395
Hrant <hrant.simonyan.2018@gmail.com>
2024-08-23 16:00:27 +0400

    add new images for gods theme

--- frontend/src/data/initialCards2.ts
+++ frontend/src/data/initialCards2.ts
@@ -2,7 +2,23 @@ import { CardType } from '@/types/commonTypes';
 import god1 from '@/assets/god-1.jpg';
 import god2 from '@/assets/god-2.jpg';
 import god3 from '@/assets/god-3.jpg';
+import god4 from '@/assets/god-4.jpg';
+import god5 from '@/assets/god-5.jpg';
+import god6 from '@/assets/god-6.jpg';
+import god7 from '@/assets/god-7.jpg';
+import god8 from '@/assets/god-8.jpg';
+import god9 from '@/assets/god-9.jpg';
+import god10 from '@/assets/god-10.jpg';
+import god11 from '@/assets/god-11.jpg';
+import god12 from '@/assets/god-12.jpg';
+import god13 from '@/assets/god-13.jpg';
+import god14 from '@/assets/god-14.jpg';
+import god15 from '@/assets/god-15.jpg';
+import god16 from '@/assets/god-16.jpg';
+import god17 from '@/assets/god-17.jpg';
+import god18 from '@/assets/god-18.jpg';
 import joker from '@/assets/joker-2.jpg';
+import freeze from '@/assets/freeze-2.jpg';
 
 export const initialCards2: CardType[] = [
   {
@@ -34,11 +50,155 @@ export const initialCards2: CardType[] = [
   },
   {
     id: 4,
+    image: god4,
+    alt: 'memory game card',
+    isFlipped: false,
+    isMatched: false,
+    isJoker: false,
+    isFreeze: false,
+  },
+  {
+    id: 5,
+    image: god5,
+    alt: 'memory game card',
+    isFlipped: false,
+    isMatched: false,
+    isJoker: false,
+    isFreeze: false,
+  },
+  {
+    id: 6,
+    image: god6,
+    alt: 'memory game card',
+    isFlipped: false,
+    isMatched: false,
+    isJoker: false,
+    isFreeze: false,
+  },
+  {
+    id: 7,
+    image: god7,
+    alt: 'memory game card',
+    isFlipped: false,
+    isMatched: false,
+    isJoker: false,
+    isFreeze: false,
+  },
+  {
+    id: 8,
+    image: god8,
+    alt: 'memory game card',
+    isFlipped: false,
+    isMatched: false,
+    isJoker: false,
+    isFreeze: false,
+  },
+  {
+    id: 9,
+    image: god9,
+    alt: 'memory game card',
+    isFlipped: false,
+    isMatched: false,
+    isJoker: false,
+    isFreeze: false,
+  },
+  {
+    id: 10,
+    image: god10,
+    alt: 'memory game card',
+    isFlipped: false,
+    isMatched: false,
+    isJoker: false,
+    isFreeze: false,
+  },
+  {
+    id: 11,
+    image: god11,
+    alt: 'memory game card',
+    isFlipped: false,
+    isMatched: false,
+    isJoker: false,
+    isFreeze: false,
+  },
+  {
+    id: 12,
+    image: god12,
+    alt: 'memory game card',
+    isFlipped: false,
+    isMatched: false,
+    isJoker: false,
+    isFreeze: false,
+  },
+  {
+    id: 13,
+    image: god13,
+    alt: 'memory game card',
+    isFlipped: false,
+    isMatched: false,
+    isJoker: false,
+    isFreeze: false,
+  },
+  {
+    id: 14,
+    image: god14,
+    alt: 'memory game card',
+    isFlipped: false,
+    isMatched: false,
+    isJoker: false,
+    isFreeze: false,
+  },
+  {
+    id: 15,
+    image: god15,
+    alt: 'memory game card',
+    isFlipped: false,
+    isMatched: false,
+    isJoker: false,
+    isFreeze: false,
+  },
+  {
+    id: 16,
+    image: god16,
+    alt: 'memory game card',
+    isFlipped: false,
+    isMatched: false,
+    isJoker: false,
+    isFreeze: false,
+  },
+  {
+    id: 17,
+    image: god17,
+    alt: 'memory game card',
+    isFlipped: false,
+    isMatched: false,
+    isJoker: false,
+    isFreeze: false,
+  },
+  {
+    id: 18,
+    image: god18,
+    alt: 'memory game card',
+    isFlipped: false,
+    isMatched: false,
+    isJoker: false,
+    isFreeze: false,
+  },
+  {
+    id: 19,
     image: joker,
     alt: 'memory game card',
     isFlipped: false,
     isMatched: false,
     isJoker: true,
     isFreeze: false,
+  },
+  {
+    id: 20,
+    image: freeze,
+    alt: 'memory game card',
+    isFlipped: false,
+    isMatched: false,
+    isJoker: false,
+    isFreeze: true,
   },
 ];
