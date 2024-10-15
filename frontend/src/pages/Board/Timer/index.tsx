0d13014aa848ada6a71dbd56b3ab053d8f213a6d
Hrant <hrant.simonyan.2018@gmail.com>
2024-10-15 12:20:59 +0400

    create and add win modal

--- frontend/src/pages/Board/Timer/index.tsx
+++ frontend/src/pages/Board/Timer/index.tsx
@@ -22,53 +22,53 @@ const Timer = () => {
     resetMatchedCards,
     shuffleAndReset,
   } = useGame();
-  const nextLevel = Number(level) + 1;
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  // const nextLevel = Number(level) + 1;
+  // const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
+  // const closeModal = () => {
+  //   setIsModalOpen(false);
+  // };
 
-  const updateUser = async ({ userId, updateData }: any) => {
-    const response = await mutateAsync({ userId, updateData });
-    console.log(response, 'response');
-    localStorage.setItem(
-      'user',
-      JSON.stringify({
-        id: response.updatedUser._id,
-        username: response.updatedUser.username,
-        level: response.updatedUser.level,
-        count: response.updatedUser.count,
-      })
-    );
-  };
+  // const updateUser = async ({ userId, updateData }: any) => {
+  //   const response = await mutateAsync({ userId, updateData });
+  //   console.log(response, 'response');
+  //   localStorage.setItem(
+  //     'user',
+  //     JSON.stringify({
+  //       id: response.updatedUser._id,
+  //       username: response.updatedUser.username,
+  //       level: response.updatedUser.level,
+  //       count: response.updatedUser.count,
+  //     })
+  //   );
+  // };
 
-  const onNextLevel = () => {
-    resetFlippedCards();
-    resetMatchedCards();
-    shuffleAndReset();
-    updateUser({ userId: user.id, updateData: { level: 3, count: 500 } });
-    setTimeout(() => {
-      navigate(`/levels/list/${theme}/${difficulty}/${nextLevel}`);
-    });
-    if (difficulty !== 'easy') {
-      getNextLevelTime();
-    }
-    setIsModalOpen(false);
-  };
+  // const onNextLevel = () => {
+  //   resetFlippedCards();
+  //   resetMatchedCards();
+  //   shuffleAndReset();
+  //   updateUser({ userId: user.id, updateData: { count: 500 } });
+  //   setTimeout(() => {
+  //     navigate(`/levels/list/${theme}/${difficulty}/${nextLevel}`);
+  //   });
+  //   if (difficulty !== 'easy') {
+  //     getNextLevelTime();
+  //   }
+  //   setIsModalOpen(false);
+  // };
 
-  useEffect(() => {
-    const requiredMatches =
-      difficulty === 'veryhard'
-        ? getRequiredMatches(Number(level), difficulty) + 2
-        : getRequiredMatches(Number(level), difficulty);
-    if (matchedCards.length === requiredMatches) {
-      setIsModalOpen(true);
-      if (difficulty !== 'easy') {
-        pauseGame();
-      }
-    }
-  }, [matchedCards, level]);
+  // useEffect(() => {
+  //   const requiredMatches =
+  //     difficulty === 'veryhard'
+  //       ? getRequiredMatches(Number(level), difficulty) + 2
+  //       : getRequiredMatches(Number(level), difficulty);
+  //   if (matchedCards.length === requiredMatches) {
+  //     setIsModalOpen(true);
+  //     if (difficulty !== 'easy') {
+  //       pauseGame();
+  //     }
+  //   }
+  // }, [matchedCards, level]);
   return (
     <>
       {difficulty !== 'easy' && (
@@ -76,34 +76,6 @@ const Timer = () => {
           <TimerButtons />
         </div>
       )}
-      <Modal isOpen={isModalOpen} onClose={closeModal} isVisible={false}>
-        {nextLevel > 5 ? (
-          <>
-            <h1>Congratulations!</h1>
-            <p>You have completed all levels.</p>
-            <div className={styles.buttons_block}>
-              <Button className={styles.button} onClick={() => navigate('/')}>
-                Home Page
-              </Button>
-            </div>
-          </>
-        ) : (
-          <>
-            <h1>You Win</h1>
-            <div className={styles.buttons_block}>
-              <Button
-                className={styles.button}
-                onClick={() => navigate(`/levels/list/${theme}/${difficulty}/`)}
-              >
-                Back to List
-              </Button>
-              <Button className={styles.button} onClick={() => onNextLevel()}>
-                Next Level
-              </Button>
-            </div>
-          </>
-        )}
-      </Modal>
     </>
   );
 };
---
+++ frontend/src/pages/Board/WinModal/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { IWinModalProps } from './types';
+import { Button, Modal } from '@/components';
+
+import styles from './index.module.css';
+
+const WinModal = ({ isModalOpen, closeModal, nextLevel, onNextLevel }: IWinModalProps) => {
+  const { theme, difficulty } = useParams();
+  const navigate = useNavigate();
+
+  return (
+    <Modal isOpen={isModalOpen} onClose={closeModal} isVisible={false}>
+      {nextLevel > 5 ? (
+        <>
+          <h1>Congratulations!</h1>
+          <p>You have completed all levels.</p>
+          <div className={styles.buttons_block}>
+            <Button className={styles.button} onClick={() => navigate('/')}>
+              Home Page
+            </Button>
+          </div>
+        </>
+      ) : (
+        <>
+          <h1>You Win</h1>
+          <div className={styles.buttons_block}>
+            <Button
+              className={styles.button}
+              onClick={() => navigate(`/levels/list/${theme}/${difficulty}/`)}
+            >
+              Back to List
+            </Button>
+            <Button className={styles.button} onClick={() => onNextLevel()}>
+              Next Level
+            </Button>
+          </div>
+        </>
+      )}
+    </Modal>
+  );
+};
+
+export default React.memo(WinModal);
